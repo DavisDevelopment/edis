b9f1688b778271502ee3df52df81f156d2918b5c
DavisDevelopment <epikit@gmail.com>
2017-11-15 15:50:14 -0600

    added 'now' method to edis.Globals

--- edis/Globals.hx
+++ edis/Globals.hx
@@ -1,5 +1,8 @@
 package edis;
 
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.ds.tuples.*;
 import tannus.math.TMath.*;
 
 using StringTools;
@@ -7,6 +10,7 @@ using tannus.ds.StringUtils;
 using Slambda;
 using tannus.ds.ArrayTools;
 using tannus.math.TMath;
+using tannus.FunctionTools;
 
 class Globals {
     public static function defer(action:Void->Void):Void {
@@ -16,6 +20,19 @@ class Globals {
             untyped __js__('setTimeout({0}, 0)', action);
         #end
     }
+
+    public static function now():Float {
+        #if window
+            return window.performance.now();
+        #else
+            var hrtime:Array<Int> = process.hrtime();
+            return (switch ( hrtime ) {
+                case [seconds, nanoseconds]: ((seconds * 1e3) + (nanoseconds / 1e6));
+                case _: throw 'TypeError: Invalid result from `process.hrtime()`';
+            });
+        #end
+    }
+
 #if underscore
     public static var us(get, never):Dynamic;
     private static inline function get_us() return _;
