cc6e89f577085e1cdd1c515f03b1df26ce25533e
DavisDevelopment <epikit@gmail.com>
2018-01-04 05:24:29 -0600

    corrected minor semantic error in edis.concurrency.NodeBoss

--- edis/concurrency/NodeBoss.hx
+++ edis/concurrency/NodeBoss.hx
@@ -8,11 +8,13 @@ class NodeBoss extends Boss {
     public function new(cp : ChildProcess):Void {
         super();
         p = cp;
+        init();
     }
 
-    override function send(type:String, data:Dynamic, encoding:WorkerPacketEncoding=None):Void {
-        p.send(packet(type, data, encoding));
-    }
+    override function _post(data: Dynamic) p.send( data );
+    //override function send(type:String, data:Dynamic, encoding:WorkerPacketEncoding=None):Void {
+        //p.send(packet(type, data, encoding));
+    //}
 
     override function kill():Void p.kill();
 
