e6ae4844c046e08174abba4f3b56aa65e97024ca
DavisDevelopment <epikit@gmail.com>
2018-01-25 15:31:21 -0600

    finished rough initial implementation of edis.storage.fs.async.ReadStreamImpl

--- edis/storage/fs/async/ReadStreamImpl.hx
+++ edis/storage/fs/async/ReadStreamImpl.hx
@@ -12,6 +12,9 @@ class ReadStreamImpl<T> {
     /* Constructor Function */
     public function new():Void {
         __onPacket = new Signal();
+        __chunkBuffer = new Array();
+        __allowBuffering = true;
+        __status = SSUnbound;
     }
 
 /* === Instance Methods === */
@@ -19,10 +22,37 @@ class ReadStreamImpl<T> {
     /**
       * initiates the stream
       */
-    public function open(?callback : Cb<RSPacket<T>>):Void {
-        if (callback != null)
-            __listen( callback );
+    public function open():Void {
+        onData(function(chunk: T) {
+            if ( __allowBuffering ) {
+                //__chunkBuffer.push( chunk );
+                __buffer( chunk );
+            }
+        });
         __start();
+    }
+
+    /**
+      * pause [this] Stream
+      */
+    public function pause():Void {
+        __pause();
+    }
+
+    public function isPaused():Bool {
+        return __isPaused();
+    }
+
+    public function resume():Void {
+        __resume();
+    }
+
+    public function setBuffering(status: Bool):Void {
+        __allowBuffering = status;
+    }
+
+    public function flush():Void {
+        __flush();
     }
 
     /**
@@ -32,7 +62,11 @@ class ReadStreamImpl<T> {
         __destroy();
     }
 
-    public function onData(f:T->Void, once:Bool=false):Void {
+    public function getStatus():RStreamStatus {
+        return __getStatus();
+    }
+
+    public function onData(f:T->Void, ?once:Bool=false):Void {
         __listen(function(?error, ?packet) {
             if (error == null && packet != null) {
                 switch ( packet ) {
@@ -45,7 +79,7 @@ class ReadStreamImpl<T> {
         }, once);
     }
 
-    public function onError(f:Dynamic->Void, once:Bool=false):Void {
+    public function onError(f:Dynamic->Void, ?once:Bool=false):Void {
         __listen(function(?error, ?packet) {
             if (error != null) {
                 f( error );
@@ -61,18 +95,93 @@ class ReadStreamImpl<T> {
         }, once);
     }
 
+    public function onEnd(f: Void->Void):Void {
+        __listen(function(?error, ?packet) {
+            if (error == null && packet != null) {
+                switch ( packet ) {
+                    case RSPEnd:
+                        f();
+
+                    default: null;
+                }
+            }
+        });
+    }
+
+    public function onClose(f: Void->Void):Void {
+        __listen(function(?error, ?packet) {
+            if (error == null && packet != null) {
+                switch ( packet ) {
+                    case RSPClose:
+                        f();
+
+                    default: null;
+                }
+            }
+        });
+    }
+
     /**
       * internal method that actually starts the streaming process
       */
     private function __start():Void {
+        //throw 'not implemented';
+    }
+
+    private function __pause():Void throw 'not implemented';
+    private function __isPaused():Bool throw 'not implemented';
+    private function __resume():Void throw 'not implemented';
+
+    private function __flush():Void {
+        var chunk: T;
+
+        while (__chunkBuffer.length > 0) {
+            chunk = __chunkBuffer.shift();
+            __send( chunk );
+        }
+    }
+
+    private function __buffer(chunk: T):Void {
+        __chunkBuffer.push( chunk );
+    }
+
+    private function __destroy():Void {
         throw 'not implemented';
+    }
+
+    private function __getStatus():RStreamStatus {
+        switch ( __status ) {
+            case SSUnbound, SSBuffered:
+                return __status;
+
+            case SSOpen:
+                if (isPaused()) {
+                    if (__allowBuffering && __chunkBuffer.length > 0) {
+                        return SSBuffered;
+                    }
+                    else {
+                        return SSPaused;
+                    }
+                } else return SSOpen;
+
+            case SSEnded, SSClosed:
+                if (__allowBuffering && __chunkBuffer.length > 0) {
+                    return SSBuffered;
+                }
+                else {
+                    return __status;
+                }
+
+            case _:
+                throw 'betty';
+        }
     }
 
     /**
       * attach a callback for incoming 'packet's
       */
     private function __listen(callback:Cb<RSPacket<T>>, once:Bool=false):Void {
-        (once?__onPacket.once:__onPacket.on)(function(packet) {
+        __onPacket.on(function(packet) {
             switch ( packet ) {
                 case RSPError(error):
                     callback(error, null);
@@ -80,22 +189,29 @@ class ReadStreamImpl<T> {
                 case other:
                     callback(null, other);
             }
-        });
+        }, once);
     }
 
     // 'send' a packet
-    private inline function pkt(packet: RSPacket<T>):Void {
-        __onPacket.call()
+    private function pkt(packet: RSPacket<T>):Void {
+        __onPacket.call( packet );
     }
 
     // send a 'close' signal
     private function __close():Void {
         pkt( RSPClose );
+        __status = SSClosed;
+    }
+
+    private function __end():Void {
+        pkt( RSPEnd );
+        __status = SSEnded;
     }
 
     // send data
-    private unction __send(data : T):Void {
-        pkt(RSPData( data ));
+    private function __send(data : T):Void {
+        __status = SSOpen;
+        pkt(RSPData(__itransform( data )));
     }
     
     // raise an error
@@ -103,50 +219,33 @@ class ReadStreamImpl<T> {
         pkt(RSPError( error ));
     }
 
+    // we might perform data transformation here
+    private function __itransform(data: T):T {
+        return data;
+    }
+
 /* === Instance Fields === */
 
     //private var __onPacket:Null<Cb<RSPacket<T>>>;
     private var __onPacket: Signal<RSPacket<T>>;
+    private var __chunkBuffer: Array<T>;
+    private var __allowBuffering: Bool;
+
+    private var __status: RStreamStatus;
 }
 
 enum RSPacket<T> {
     RSPData(data : T);
     RSPError(error: Dynamic);
     RSPClose;
+    RSPEnd;
 }
 
-class ReadStreamTransformer<TIn, TOut> extends ReadStream<TOut> {
-    private var _src : ReadStreamImpl<TIn>;
-
-    public function new(src : ReadStreamImpl<TIn>):Void {
-        super();
-
-        _src = src;
-    }
-
-    override function __start():Void {
-        _src.open( __mapper );
-    }
-
-    private function __mapper(?error:Dynamic, ?packet:RSPacket<TIn>):Void {
-        if (error != null) {
-            __raise( error );
-        }
-        else if (packet != null) {
-            switch ( packet ) {
-                case RSPData( in_data ):
-                    __send(__transform( in_data ));
-
-                case RSPClose:
-                    __close();
-            }
-        }
-        else {
-            throw 'Error: Neither an error nor a packet was provided';
-        }
-    }
-
-    private function __transform(in_data : TIn):TOut {
-        throw 'not implemented';
-    }
+enum RStreamStatus {
+    SSUnbound;
+    SSOpen;
+    SSPaused;
+    SSBuffered;
+    SSClosed;
+    SSEnded;
 }
