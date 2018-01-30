ffd922bb3ad8435712ba76f227c7339d770e0389
DavisDevelopment <epikit@gmail.com>
2018-01-29 21:27:06 -0600

    added 'childElement' and 'text' methods to edis.xml.XmlTools

--- edis/xml/XmlTools.hx
+++ edis/xml/XmlTools.hx
@@ -21,4 +21,52 @@ class XmlTools {
     public static function isElement(xml:Xml):Bool return (xml.nodeType == Element);
     public static function isDocument(xml:Xml):Bool return (xml.nodeType == Document);
     public static function isTextual(xml:Xml):Bool return (xml.nodeType == PCData || xml.nodeType == CData || xml.nodeType == Comment);
+
+    public static function childElement(xml:Xml, nodeName:String, ?attrs:Map<String,String>, ?insertAt:Int):Xml {
+        var el:Xml = Xml.createElement( nodeName );
+        if (attrs != null) {
+            for (key in attrs.keys())
+                el.set(key, attrs[key]);
+        }
+        if (insertAt != null) {
+            xml.insertChild(el, insertAt);
+        }
+        else {
+            xml.addChild( el );
+        }
+        return el;
+    }
+
+    public static function text(xml:Xml, ?newText:String):String {
+        var setting:Bool = (newText != null);
+        switch ( xml.nodeType ) {
+            case Document, Element:
+                if ( setting ) {
+                    // empty [xml]
+                    for (node in xml) {
+                        xml.removeChild( node );
+                    }
+                    xml.addChild(Xml.createPCData( newText ));
+                    return newText;
+                }
+                else {
+                    var res:String = '';
+                    for (node in xml) {
+                        res += text( node );
+                    }
+                    return res;
+                }
+
+            case PCData:
+                if ( setting ) {
+                    throw 'Error: what are you doing?';
+                }
+                else {
+                    return xml.nodeValue;
+                }
+
+            default:
+                return '';
+        }
+    }
 }
