ba5ee6198f51212839e0d27432a9a0f170e22ab9
DavisDevelopment <epikit@gmail.com>
2018-04-18 18:33:26 -0500

    added some new wrapper-related methods to Entry

--- edis/storage/fs/async/Entry.hx
+++ edis/storage/fs/async/Entry.hx
@@ -8,6 +8,8 @@ import tannus.async.promises.*;
 
 import Slambda.fn;
 
+import edis.storage.fs.async.EntryType;
+
 import haxe.Serializer;
 import haxe.Unserializer;
 
@@ -15,12 +17,16 @@ using StringTools;
 using tannus.ds.StringUtils;
 using Slambda;
 using tannus.ds.ArrayTools;
+using tannus.async.Asyncs;
 
+/**
+  *
+  */
 class Entry {
     /* Constructor Function */
-    public function new(path:Path, name:String, isDirectory:Bool, isFile:Bool):Void {
+    public function new(path:Path, fs:FileSystem, isDirectory:Bool, isFile:Bool):Void {
         this.path = path;
-        this.name = name;
+        this.fs = fs;
         this.isDirectory = isDirectory;
         this.isFile = isFile;
         this.type = (switch ([isFile, isDirectory]) {
@@ -29,15 +35,63 @@ class Entry {
             case _: throw 'Betty: What the fuck?';
         });
 
-        js.Object.freeze( this );
+        js.Object.preventExtensions( this );
     }
 
 /* === Instance Methods === */
 
+    private inline function ew<T:EntryWrapper>(make:Entry->T, check:Entry->Bool):Null<T> {
+        if (check( this )) {
+            return make( this );
+        }
+        else {
+            return null;
+        }
+    }
+
+    /**
+      * convert [this] Entry to a Directory object
+      */
+    public function directory():Null<Directory> {
+        var dir = ew(fn(new Directory(_)), fn(_.type.match(ETDirectory)));
+        if (dir == null) {
+            throw new FileError(TypeMismatchError, '${this} is not a directory');
+        }
+        return dir;
+    }
+
+    /**
+      * convert [this] Entry to a File object
+      */
+    public function file():Null<File> {
+        var f:Null<File> = ew(fn(new File(_)), fn(_.type.match(ETFile)));
+        if (f == null) {
+            throw new FileError(TypeMismatchError, '${this} is not a valid file');
+        }
+        return f;
+    }
+
+    /**
+      * convert [this] to its 'wrapped' type
+      */
+    public function wrapped():WrappedEntryType {
+        return (switch ( type ) {
+            case ETFile: ETFile(file());
+            case ETDirectory: ETDirectory(directory());
+        });
+    }
+
+    /**
+      * display [this] as a String
+      */
+    public function toString():String {
+        return 'Entry("$path")';
+    }
+
 /* === Instance Fields === */
 
     public var path : Path;
-    public var name : String;
+    public var fs : FileSystem;
     public var isDirectory : Bool;
     public var isFile : Bool;
     public var type : EntryType;
